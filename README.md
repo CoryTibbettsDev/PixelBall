d47b30da91d34c6b99f70a8e7eb68bb30929ac55
user <user@example.com>
2021-01-11 16:56:02 -0500

    Finally got around to fixing 404 and 500 errors

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #### A web browser based multi-player game designed to be an advanced version of Pong
 
 #### Code written while I was learning JS I would not recommend copying it
+
+#### Mine comments suck and probably do not make sense to anyone but me
--- src/index.js
+++ src/index.js
@@ -28,14 +28,23 @@ export const io = socket(server)
 // https://nodejs.org/api/esm.html#esm_no_filename_or_dirname
 app.use(express.static('public'))
 
-// Express 404 error response https://expressjs.com/en/starter/faq.html
-app.use(function (req, res, next) {
-  res.status(404).send('404 error')
-})
+// Need absolute filepath for res.sendFile() but cannot use __dirname because
+// I am using ES6 modules use this instead
+// https://stackoverflow.com/questions/46745014/
+// alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// Can do either of these now
+// res.sendFile(__dirname + '/index.html');
+// res.sendFile('index.html', { root: __dirname });
 
-// Express 500 error response https://expressjs.com/en/starter/faq.html
+// Express 404 and 500 error response https://expressjs.com/en/starter/faq.html
 app.use(function (req, res, next) {
-  res.status(500).send('500 error, something broke!')
+	res.status(404).sendFile(__dirname + '/public/404.html')
+})
+app.use(function (req, res, next) {
+	res.status(500).sendFile(__dirname + '/public/500.html')
 })
 
 import { playerConnect } from './game_server.js'
